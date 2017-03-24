c34f5da79e7316bae1cfc27300c5cb02817a73cf
Josean <josean7jk@gmail.com>
2017-03-24 14:39:35 +0100

    idao

---
+++ src/IDAO.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Josean on 24/03/2017.
+ */
+public interface IDAO {
+
+    public boolean insertUserDB(String id, String name, String pw);
+    public void updateuser(String id, String newId, String newName, String newPw);
+    public void selectuser (String id);
+    public void selectAllUsersDB();
+
+}
